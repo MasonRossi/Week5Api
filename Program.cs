36743216308f954162a6406acdae0f2d67df7413
MasonRossi <Mrossi0770@conestogac.on.ca>
2026-02-12 15:57:38 -0500

    Initial commit of Week 5 API

---
+++ Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGet("/hello", () => "Hello from your Azure API running .NET 10");
+
+app.Run();
